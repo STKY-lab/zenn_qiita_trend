c1cc96e1179abfd3b459a563142e1269bed0d9b6
山本　堅心 <s2032149nb@s.chibakoudai.jp>
2023-03-03 13:05:27 +0900

    Add: freezed

---
+++ lib/models/zenn_model.dart
@@ -0,0 +1,21 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
+
+part 'zenn_model.freezed.dart';
+part 'zenn_model.g.dart';
+
+@freezed
+class ZennModel with _$ZennModel {
+  factory ZennModel({
+    required String title,
+    required int commentCount,
+    required int likedCount,
+    required String emoji,
+    required String updatedAt,
+    required String path,
+    required Map<String, dynamic> user,
+  }) = _ZennModel;
+
+  factory ZennModel.fromJson(Map<String, dynamic> json) =>
+      _$ZennModelFromJson(json);
+}
